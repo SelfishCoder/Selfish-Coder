8d9604f3122207a81f07115967c1ddfd0640e662
Selfish Coder <meric.balikci@gmail.com>
2021-10-01 21:46:43 +0300

    Object Pooling Design Pattern has been added.

---
+++ Common/Object Pool/Pool.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PoolManagement
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Pool
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MonoBehaviour Prefab { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int InitialSize { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MaximumSize { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentSize { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Expendable { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Initialized { get; protected set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Transform Parent { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Pool()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prefab"></param>
+        protected Pool(MonoBehaviour prefab)
+        {
+            Prefab = prefab;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract void Init();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public abstract MonoBehaviour Request();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract void Clear();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="returnedObject"></param>
+        public abstract void Return(MonoBehaviour returnedObject);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Pool<T> : Pool where T : MonoBehaviour
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<T> Objects { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public new T Prefab { get; protected set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Pool(T prefab) : base(prefab)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <param name="initialSize"></param>
+        /// <param name="maximumSize"></param>
+        /// <param name="extendable"></param>
+        /// <param name="autoInitialize"></param>
+        public Pool(T prefab, int initialSize, int maximumSize, bool extendable = true, bool autoInitialize = true) : base(prefab)
+        {
+            this.Prefab = prefab;
+            this.InitialSize = initialSize;
+            this.MaximumSize = maximumSize;
+            this.Expendable = extendable;
+            this.Parent = new GameObject($"{Prefab.GetType().Name} Pool").transform;
+            if (autoInitialize) Init();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Init()
+        {
+            if (Initialized) return;
+
+            Objects = new List<T>();
+            for (int i = 0; i < InitialSize; i++)
+            {
+                T clone = Object.Instantiate(Prefab, Parent, true);
+                clone.gameObject.SetActive(false);
+                Objects.Add(clone);
+                CurrentSize = InitialSize;
+            }
+
+            Initialized = true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override MonoBehaviour Request()
+        {
+            if (!Initialized) Init();
+
+            foreach (T poolObject in Objects.Where(poolObject => !poolObject.gameObject.activeInHierarchy))
+            {
+                poolObject.gameObject.SetActive(true);
+                return poolObject;
+            }
+
+            if (CurrentSize == MaximumSize && !Expendable) return null;
+
+            T newPoolObject = Extend();
+            newPoolObject.gameObject.SetActive(true);
+            return newPoolObject;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="returnedObject"></param>
+        public override void Return(MonoBehaviour returnedObject)
+        {
+            returnedObject.gameObject.SetActive(false);
+            returnedObject.transform.SetParent(Parent);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Clear()
+        {
+            foreach (T monoBehaviour in Objects)
+            {
+                Object.Destroy(monoBehaviour);
+            }
+
+            Objects.Clear();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private T Extend()
+        {
+            T poolObject = CreateObject();
+            MaximumSize = CurrentSize;
+            return poolObject;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private T CreateObject()
+        {
+            T poolObject = Object.Instantiate(Prefab, Parent);
+            Objects.Add(poolObject);
+            poolObject.gameObject.SetActive(false);
+            CurrentSize++;
+            return poolObject;
+        }
+    }
+}
---
+++ Common/Object Pool/PoolManager.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using UnityEngine;
+using SelfishCoder.Core;
+using System.Collections.Generic;
+using System.Linq;
+using Object = UnityEngine.Object;
+
+namespace PoolManagement
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PoolManager : IManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Dictionary<Type, Pool> Pools { get; private set; } = default;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Initialized { get; private set; } = default;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Init()
+        {
+            Pools = new Dictionary<Type, Pool>();
+            Initialized = true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void LateInit()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        public void Add(Pool pool)
+        {
+            if (!Initialized) Init();
+            if (IsNull(pool) || Contains(pool.GetType())) return;
+            Pools.Add((pool.Prefab).GetType(), pool);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        public void Remove(Pool pool)
+        {
+            if (Contains(pool))
+            {
+                Pools.Remove(GetPoolType(pool));
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <param name="initialSize"></param>
+        /// <param name="maxSize"></param>
+        /// <param name="extendable"></param>
+        /// <param name="autoInitialize"></param>
+        /// <returns></returns>
+        public Pool Create(MonoBehaviour prefab, int initialSize, int maxSize, bool extendable = true, bool autoInitialize = true)
+        {
+            Type genericPoolType = typeof(Pool<>);
+            Type[] typeArgs = new Type[] {prefab.GetType()};
+            Type poolType = genericPoolType.MakeGenericType(typeArgs);
+            object[] args = {prefab, initialSize, maxSize, extendable, autoInitialize};
+            Pool pool = Activator.CreateInstance(poolType, args) as Pool;
+            Add(pool);
+            return pool;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public bool Contains<T>() where T : MonoBehaviour
+        {
+            return Pools.ContainsKey(typeof(T));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public bool Contains(Type type)
+        {
+            return Pools.ContainsKey(type);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <returns></returns>
+        public bool Contains(Pool pool)
+        {
+            return Pools.ContainsValue(pool);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <returns></returns>
+        public bool IsNull(Pool pool)
+        {
+            return pool == null;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <returns></returns>
+        public bool IsNullOrEmpty(Pool pool)
+        {
+            return pool == null || !pool.Initialized;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <returns></returns>
+        public Type GetPoolType(Pool pool)
+        {
+            return pool.Prefab.GetType();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public Pool TryGetPool(Type type)
+        {
+            return !Contains(type) ? null : Pools[type];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public Pool<T> TryGetPool<T>() where T : MonoBehaviour
+        {
+            if (!Contains(typeof(T))) return null;
+            return (Pool<T>) Pools[typeof(T)];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pool"></param>
+        public void DestroyPool(Pool pool)
+        {
+            pool.Clear();
+            Object.Destroy(pool.Parent.gameObject);
+            Remove(pool);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ClearAll()
+        {
+            foreach (Pool pool in Pools.Values)
+            {
+                DestroyPool(pool);
+            }
+            Pools.Clear();
+        }
+    }
+}
