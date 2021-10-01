1ec16a68cf3b748a3050061f24389518e12ed037
Selfish Coder <meric.balikci@gmail.com>
2021-10-01 21:45:52 +0300

    Singleton Design Pattern has been added.

---
+++ Common/Singleton/Singleton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace SelfishCoder.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected static T current;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static T Current
+        {
+            get
+            {
+                if (current) return current;
+                current = FindObjectOfType<T>();
+                if (!current)
+                {
+                    current = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+                return current;
+            }
+            protected set
+            {
+                if (current)
+                {
+                    Destroy(value.gameObject);
+                    return;
+                }
+
+                current = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual void Awake()
+        {
+            Current = this as T;
+        }
+    }
+}
