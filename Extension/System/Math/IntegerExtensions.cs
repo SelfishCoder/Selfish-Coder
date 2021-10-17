f7777cecb46abf977eb7f883a1413d05720d03c2
Selfish Coder <meric.balikci@gmail.com>
2021-10-17 15:01:06 +0300

    Integer extensions have been implemented.

---
+++ Extension/System/Math/IntegerExtensions.cs
@@ -0,0 +1,52 @@
+namespace SelfishCoder.Extensions.System
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool LessThan(this int a, int b)
+        {
+            return a < b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool LessThanOrEqual(this int a, int b)
+        {
+            return a <= b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool GreaterThan(this int a, int b)
+        {
+            return a > b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool GreaterThanOrEqual(this int a, int b)
+        {
+            return a >= b;
+        }
+    }
+}
